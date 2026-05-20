034e85cfdcde3304c96fecda06d50526071f0be8
Jayden Anokye <jaydenanokye@webdev07.local>
2026-05-20 13:42:46 -0400

    creation of models. work in progress, but all tables are now in model format, though changes are needed

--- cosmokiosk/catalog/models.py
+++ cosmokiosk/catalog/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+from django.urls import reverse # used  in get_absolute_url to get the URL for a specified ID
+from django.db.models import UniqueConstraint
+from django.db.models.functions import Lower
+import uuid
+from django.urls import reverse # used  in get_absolute_url to get the URL for a specified ID
+from django.db.models import UniqueConstraint
+from django.db.models.functions import Lower
+from django.utils import timezone
+import uuid
+
+# Create your models here.
 
 # Create your models here.
 
@@ -11,6 +22,38 @@ class Checking(models.model):
         return self.name
 
 
-
 class Client_Waiver(models.Model):
-    First_Name = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_time = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}, signed {self.date_time}"
+        
+    
+    def get_absolute_url(self):
+        return reverse('client-info', args=[str(self.id)])
+    
+    
+
+class Feedback_Questions(models.Model):
+    question = models.IntegerField(unique=True)
+    question_text = models.CharField(100)
+    
+    def __str__(self):
+        return f"Question {self.question}: {self.question_text}"
+    
+    
+class Feedback(models.Models):
+    feedback_answer = models.CharField(200)
+    
+    def __str__(self):
+        return f"Q{self.Feedback_Questions.question}: {self.feedback_answer}"
+    
+    
+class Waxing_Waiver(models.Models):
+    answer = models.CharField(100)
+
+
+class Services(models.Models):
+    service_name = models.CharField(100)
