ced520994a9681f177bb3d22e682642f30d37841
Jayden Anokye <jaydenanokyecats@gmail.com>
2026-05-29 08:21:03 -0400

    progress push #1

--- cosmokiosk/catalog/forms.py
+++ cosmokiosk/catalog/forms.py
@@ -1,9 +1,61 @@
 from django import forms
 from .models import Client_Waiver, Waxing_Waiver, Feedback_Questions, Feedback, Services
 from django.core.exceptions import ValidationError
+import datetime
+from django.utils.translation import gettext_lazy as _
 
-class ClientWaiverForm(forms.ModelForm):
+
+
+class Client_Waiver(forms.ModelForm):
     class Meta:
         model = Client_Waiver
-        fields = set([field.name for field in Client_Waiver._meta.fields if field.name != 'id'])        
+        fields = ['first_name', 'last_name', 'date_time']
+
+    def clean_date(self):
+         date = self.cleaned_data['date_time']
+
+         if date.date() < datetime.date.today():
+             raise ValidationError(_('Invalid - date is in the past '))
+         
+         return date
     
+    def clean_first_name(self):
+        first_name = self.cleaned_date.get('first_name', '').strip()
+        if not first_name:
+            raise ValidationError('Name fields cannot be empty.')
+        
+        for char in first_name:
+            if not (char.isalpha() or char.isspace()):
+                raise ValidationError('First name can only contain letters.')
+            
+        return first_name
+    
+    
+    def clean_first_name(self):
+        last_name = self.cleaned_date.get('last_name', '').strip()
+        if not last_name:
+            raise ValidationError('Name fields cannot be empty.')
+        
+        for char in last_name:
+            if not (char.isalpha() or char.isspace()):
+                raise ValidationError('Last name can only contain letters.')
+            
+        return last_name
+    
+    
+    
+class Waxing_Waiver(forms.ModelForm):
+    class Meta:
+        model = Waxing_Waiver
+        fields = ['medicine', 'allergy', 'soap_use', 'exposed', 'health_issues', 'timestamp', 'agreement', 'client_info']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        boolean_fields = ['medicine', 'allergy', 'soap_use', 'exposed', 'health_issues', 'timestamp', 'agreement', 'client_info']
+    
+
+
+        for field in boolean_fields:
+            if not cleaned_data.get(field):
+                self.add_error(field,_('Please check every box to confirm your waiver agreement'))
+        return cleaned_data        
--- cosmokiosk/catalog/views.py
+++ cosmokiosk/catalog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import Waxing_Waiver, Client_Waiver
+
 
 # Create your views here.
 def index(request):
@@ -16,3 +17,30 @@ def welcome_page(request):
     
 def services_page(request):
     return render(request, 'catalog/services.html')
+
+
+
+
+#this is the place for all the forms stuff
+
+def waiver_view(request):
+    if request.method == "POST":
+        form = Waxing_Waiver(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect
+    else:
+        form = Waxing_Waiver()
+    return render(request, 'waiver_template.html', {'form':form})
+
+def client_view(request):
+    if request.method == "POST":
+        form = Client_Waiver(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect
+    else:
+        form = Client_Waiver()
+    return render(request, 'sign-in.html', {'form':form})
